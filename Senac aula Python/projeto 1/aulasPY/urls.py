8b99b65a515affbbc988b65441c667a49127c1c8
gustavosuperplay <146247844+gustavosuperplay@users.noreply.github.com>
2024-02-07 12:58:39 -0400

    ok

--- Senac aula Python/projeto 1/aulasPY/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from aulasPY import views as views_aulasPY
-
-urlapatterns = [
-    path('', views_aulasPY.nomedafuncaoview, name='view1'),
-   
-]
--- Senac aula Python/projeto 1/aulasPY/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def nomedafuncaoview (request):
-    return render(
-        request,
-        'aulasPY/arquivo.html'
-    )
